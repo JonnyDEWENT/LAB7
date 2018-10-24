793e8a8792f5ba7cff6bf583a733e9a700dc5b6e
JonnyDEWENT <32647918+JonnyDEWENT@users.noreply.github.com>
2018-10-24 18:41:24 -0400

    added max sem count and max shm size

--- resource.c
+++ resource.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <sys/sem.h>
+
+
 
 int main(int argv, char** argc){
 
    long pageSize;
    long physPages;
    long maxProcess;
-
+   int shmId;
    struct rlimit rlim;
 
+   sem_t sem;
+
+   int* semval = malloc(sizeof(int));
+   semval[0] = 0;
+   int semid = semget(IPC_PRIVATE,1,00600);                                    \
+
+   struct sembuf waitsem[1];
+
+
+    waitsem[0].sem_num = 0;
+    waitsem[0].sem_op = 1;
+    waitsem[0].sem_flg = IPC_NOWAIT;
 
    pageSize = sysconf(_SC_PAGESIZE);
    printf("POSIX.1 Page Size: %ld\n",pageSize);
@@ -28,4 +50,26 @@ int main(int argv, char** argc){
    printf("(getrlimit Max number of files HARD: %ld\n", rlim.rlim_max);
 
    printf("(getrlimit) Max number of files SOFT: %ld\n", rlim.rlim_cur);
+
+   int FOO = 2147483640;
+   int size = 0;
+   while ((shmId = shmget (IPC_PRIVATE, FOO+size, IPC_CREAT|S_IRUSR|S_IWUSR)) >\
+ 0){
+     size += 1;
+     // printf("Max size = %d\n",size + 4088);                                  
+
+
+     if (shmctl (shmId, IPC_RMID, 0) < 0) {
+      perror ("can't deallocate\n");
+      exit(1);
+   }
+   }
+   printf("Max size = %d (Empirical on EOS14)\n",size + FOO - 1);
+   int count = 0;
+   while (semop(semid,&waitsem[0],1) > -1){
+     count++;
+   }
+   printf("Max count of semaphore %d (Empirical on EOS14)\n", count);
+   semctl (semid, 0, IPC_RMID);
+
 }
